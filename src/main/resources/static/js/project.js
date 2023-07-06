a15a34fe9452dd7ed59c044bc2a7e2cca6d4a272
gain00 <wjdwo1104@gmail.com>
2023-07-06 15:33:21 +0900

    회원가입 : agree 추가

---
+++ src/main/resources/static/js/project.js
@@ -0,0 +1,18 @@
+//agree
+let agree1 = document.querySelector("#agree1");
+let agree2 = document.querySelector("#agree2");
+let okagree = document.querySelector("#okagree");
+let noagree = document.querySelector("#noagree");
+
+okagree?.addEventListener('click',()=>{
+    if(!agree1.checked){
+        alert('이용약관에 동의하세요');
+    }else if(!agree2.checked){
+        alert('이용약관에 동의하세요');
+    }else{
+        location.href="/join/checkme";
+    }
+});
+noagree?.addEventListener('click',()=>{
+    location.href= "/";
+});
