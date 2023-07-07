a5bf9af842118ac46b38104cb812f9f0e920ca69
gain00 <wjdwo1104@gmail.com>
2023-07-07 12:00:32 +0900

    회원가입 : MemberMapper 단위테스트추가

---
+++ src/test/java/wjdwo1104/hello/boot/spring5boot/MemberMapperUnitTest.java
@@ -0,0 +1,37 @@
+package wjdwo1104.hello.boot.spring5boot;
+
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import org.mybatis.spring.boot.test.autoconfigure.MybatisTest;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+
+import wjdwo1104.hello.boot.spring5boot.model.Member;
+import wjdwo1104.hello.boot.spring5boot.mybatis.MemberMapper;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+
+
+@MybatisTest
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+public class MemberMapperUnitTest {
+
+    @Autowired
+    private MemberMapper memberMapper;
+
+    @Test
+    @DisplayName("MemberMapper Test")
+    void insertMember(){
+        Member m = new Member(null,"","","","","","","","",null);
+
+        int result = memberMapper.insertMember(m);
+        System.out.println(result);
+        assertEquals(result,1);
+
+    }
+
+
+}
