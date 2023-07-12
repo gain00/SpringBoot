e3b04d25f443316c5fd8eab6bcb7bf1a83e76565
gain00 <wjdwo1104@gmail.com>
2023-07-12 11:46:27 +0900

    게시판 : BoardController 페이징처리 구현

--- src/main/java/wjdwo1104/hello/boot/spring5boot/controller/BoardController.java
+++ src/main/java/wjdwo1104/hello/boot/spring5boot/controller/BoardController.java
@@ -22,9 +22,9 @@ public class BoardController {
     Logger logger = LogManager.getLogger(BoardController.class);
 
     @GetMapping("/list")
-    public String list(Model m){//moedl m? m.addattribue?
+    public String list(Model m,Integer cpg){//moedl m? m.addattribue?
         logger.info("board 호출");
-        m.addAttribute("bds",bsrv.readBoard());//bsrv.readBoard()여기서 싱행된결과가 "bds"에 담김
+        m.addAttribute("bds",bsrv.readBoard(cpg));//bsrv.readBoard()여기서 싱행된결과가 "bds"에 담김
         // bsrv.readBoard() 메서드를 호출하여 그 결과(게시판 목록)를 가져옴
         // 이 결과를 모델 객체에 "bds"라는 이름으로 추가. 이를 통해 뷰에서 이 데이터에 접근 가능
 
