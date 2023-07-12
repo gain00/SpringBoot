000f059f14f987f8d3a43c9853c4b7fe40116cd6
gain00 <wjdwo1104@gmail.com>
2023-07-12 10:15:15 +0900

    게시판 : boardDAO 단위테스트

---
+++ src/test/java/wjdwo1104/hello/boot/spring5boot/board/BoardDAOUnitTest.java
@@ -0,0 +1,41 @@
+package wjdwo1104.hello.boot.spring5boot.board;
+
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.mybatis.spring.boot.test.autoconfigure.MybatisTest;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.context.annotation.Import;
+import wjdwo1104.hello.boot.spring5boot.dao.BoardDAO;
+import wjdwo1104.hello.boot.spring5boot.dao.BoardDAOImpl;
+import wjdwo1104.hello.boot.spring5boot.dao.MemberDAO;
+import wjdwo1104.hello.boot.spring5boot.dao.MemberDAOImpl;
+import wjdwo1104.hello.boot.spring5boot.model.Board;
+import wjdwo1104.hello.boot.spring5boot.model.Member;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+
+@MybatisTest
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+@Import(BoardDAOImpl.class)
+public class BoardDAOUnitTest {
+
+    @Autowired private BoardDAO bdao;
+
+    @Test
+    @DisplayName("BoardDAO select Test")
+    void selectBoard() {
+        List<Board> results = bdao.selectBoard();
+        //System.out.println(resutls);
+        assertNotNull(results);
+    }
+
+
+
+
+}
