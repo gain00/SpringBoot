bad6f276bd59875bed2eb397841dfa571b60dc6d
gain00 <wjdwo1104@gmail.com>
2023-07-12 10:16:23 +0900

    게시판 : BoardService 단위테스트

---
+++ src/test/java/wjdwo1104/hello/boot/spring5boot/board/BoardSerivceUnitTest.java
@@ -0,0 +1,46 @@
+package wjdwo1104.hello.boot.spring5boot.board;
+
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.mybatis.spring.boot.test.autoconfigure.MybatisTest;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.context.annotation.Import;
+import wjdwo1104.hello.boot.spring5boot.dao.BoardDAOImpl;
+import wjdwo1104.hello.boot.spring5boot.dao.MemberDAOImpl;
+import wjdwo1104.hello.boot.spring5boot.model.Board;
+import wjdwo1104.hello.boot.spring5boot.model.Member;
+import wjdwo1104.hello.boot.spring5boot.service.BoardService;
+import wjdwo1104.hello.boot.spring5boot.service.BoardServiceImpl;
+import wjdwo1104.hello.boot.spring5boot.service.MemberService;
+import wjdwo1104.hello.boot.spring5boot.service.MemberServiceImpl;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+
+@MybatisTest
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+@Import({BoardServiceImpl.class, BoardDAOImpl.class})
+public class BoardSerivceUnitTest {
+
+    @Autowired
+    private BoardService bsrv;
+
+
+
+    @Test
+    @DisplayName("BoardService read Test")
+    void readBoard(){
+
+        List<Board> result = bsrv.readBoard();
+        System.out.println(result);
+        assertNotNull(result);
+
+    }
+
+
+}
