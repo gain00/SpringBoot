3b65f2f1af6a80e259c348caf19909c73552020f
gain00 <wjdwo1104@gmail.com>
2023-07-12 15:41:57 +0900

    게시판 :BoardController view 구현

--- src/main/java/wjdwo1104/hello/boot/spring5boot/controller/BoardController.java
+++ src/main/java/wjdwo1104/hello/boot/spring5boot/controller/BoardController.java
@@ -8,6 +8,7 @@ import org.apache.logging.log4j.Logger;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import wjdwo1104.hello.boot.spring5boot.service.BoardService;
 
@@ -21,16 +22,24 @@ public class BoardController {
 
     Logger logger = LogManager.getLogger(BoardController.class);
 
-    @GetMapping("/list")
-    public String list(Model m,Integer cpg){//moedl m? m.addattribue?
-        logger.info("board 호출");
+    @GetMapping("/list/{cpg}")//@pathVariable를씀으로써 매개변수 cpg의 값을 주소에 쓸수있음
+    public String list(Model m, @PathVariable Integer cpg){//moedl m? 컨트롤러는 모델이라는 타입의 객체를 파라미터로 받을수있다 m.addattribue?
+        logger.info("board/list 호출");
         m.addAttribute("bds",bsrv.readBoard(cpg));//bsrv.readBoard()여기서 싱행된결과가 "bds"에 담김
         // bsrv.readBoard() 메서드를 호출하여 그 결과(게시판 목록)를 가져옴
         // 이 결과를 모델 객체에 "bds"라는 이름으로 추가. 이를 통해 뷰에서 이 데이터에 접근 가능
-
+        m.addAttribute("cpg",cpg);//현재페이지
         return "board/list";
     }
 
+    @GetMapping("/view/{bno}")
+    public String view(Model m, @PathVariable String bno){
+        logger.info("board/view 호출");
+        m.addAttribute("bd",bsrv.readOneBoard(bno));
+
+
+        return "board/view";
+    }
 
 
 }
