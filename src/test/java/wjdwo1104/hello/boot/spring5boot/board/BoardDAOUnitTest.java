3314773319ca4e572266e2d7eacd6860b3ff7ae3
gain00 <wjdwo1104@gmail.com>
2023-07-13 16:07:45 +0900

    게시판 검색 기능 boardDAO 단위테스트

--- src/test/java/wjdwo1104/hello/boot/spring5boot/board/BoardDAOUnitTest.java
+++ src/test/java/wjdwo1104/hello/boot/spring5boot/board/BoardDAOUnitTest.java
@@ -12,7 +12,9 @@ import wjdwo1104.hello.boot.spring5boot.dao.BoardDAO;
 import wjdwo1104.hello.boot.spring5boot.dao.BoardDAOImpl;
 import wjdwo1104.hello.boot.spring5boot.model.Board;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
@@ -65,6 +67,21 @@ public class BoardDAOUnitTest {
         assertNotNull(result);
     }
 
+
+    @Test
+    @DisplayName("findBoard  Test")
+    void findBoard(){
+        Map<String, Object> params = new HashMap<>();
+        params.put("findtype","titcont");
+        params.put("findkey","기자");
+        params.put("stnum",0);
+
+        List<Board> results =  bdao.selectFindBoard(params);
+
+
+        assertNotNull(results);
+
+    }
 
 
 
