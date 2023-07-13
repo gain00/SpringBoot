22689d0082265a5da84d7d4e116171d483876b17
gain00 <wjdwo1104@gmail.com>
2023-07-13 09:50:22 +0900

    게시판 write js 작성

--- src/main/resources/static/js/project.js
+++ src/main/resources/static/js/project.js
@@ -265,7 +265,7 @@ let rmvbdbtn = document.querySelector("#rmvbdbtn");
 let lstbdbtn = document.querySelector("#lstbdbtn");
 
 newbdbtn?.addEventListener('click',()=>{
-    location.href = '/join/write';
+    location.href = '/board/write';
 });
 moddbbtn?.addEventListener('click',()=>{
     if(confirm("정말 수정하시겠어요?")){
@@ -278,5 +278,32 @@ rmvbdbtn?.addEventListener('click',()=>{
     }
 });
 lstbdbtn?.addEventListener('click',()=>{
-    location.href = '/join/list/1';
+    location.href = '/board/list/1';
 });
+
+let newbdntn = document.querySelector("#newbdntn")
+newbdntn?addEventListener('click',()=>{
+    ff
+});
+
+//board write
+let wrtbtn = document.querySelector("#wrtbtn");
+let cancelbtn3 = document.querySelector("#cancelbtn3");
+let bdfrm = document.querySelector("#bdfrm");
+
+wrtbtn?.addEventListener('click',()=>{
+    let frm = document.forms.bdfrm
+    if(frm.title.value==='') alert('제목을작성하시오');
+    else if(frm.content.value==='') alert('본문을작성하시오');
+    else if(grecaptcha.getResponse()==='') alert('c체크하시오');
+    else{
+        bdfrm.method = 'post';
+        bdfrm.submit();
+    }
+
+
+});
+cancelbtn3?.addEventListener('click',()=>{
+    location.href = '/board/list/1';
+});
+
