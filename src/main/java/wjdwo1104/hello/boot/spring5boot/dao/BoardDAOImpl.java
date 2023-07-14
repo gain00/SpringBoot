aabc9130e2e6de79743ec041202f5623e77c9a85
gain00 <wjdwo1104@gmail.com>
2023-07-14 11:02:54 +0900

    게시판 검색건수 BoardDAO 구현

--- src/main/java/wjdwo1104/hello/boot/spring5boot/dao/BoardDAOImpl.java
+++ src/main/java/wjdwo1104/hello/boot/spring5boot/dao/BoardDAOImpl.java
@@ -46,5 +46,10 @@ public class BoardDAOImpl implements BoardDAO {
         return boardMapper.selectFindBoard(params);
     }
 
+    @Override
+    public int countFindBoard(Map<String, Object> params) {
+        return boardMapper.countFindBoard(params);
+    }
+
 
 }
