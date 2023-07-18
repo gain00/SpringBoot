41f40c79a335b0d531657d773d71609d2fac86a3
gain00 <wjdwo1104@gmail.com>
2023-07-18 11:04:30 +0900

    자료실 다운로드 - PdsSerDAO : readOnePdsAttach구현

--- src/main/java/wjdwo1104/hello/boot/spring5boot/dao/PdsDAOImpl.java
+++ src/main/java/wjdwo1104/hello/boot/spring5boot/dao/PdsDAOImpl.java
@@ -47,4 +47,9 @@ public class PdsDAOImpl implements PdsDAO {
         return pdsMapper.selectOnePds(pno);
     }
 
+    @Override
+    public PdsAttach selectOnePdsAttach(String pno) {
+        return pdsMapper.selectOnePdsAttach(pno);
+    }
+
 }
