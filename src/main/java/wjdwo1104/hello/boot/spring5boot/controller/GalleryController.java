9099c3703c336d18c554c66e846245a15bfc19ff
gain00 <wjdwo1104@gmail.com>
2023-07-20 11:20:02 +0900

    갤러리 : GalleryController 추가

--- src/main/java/wjdwo1104/hello/boot/spring5boot/controller/GalleryController.java
+++ src/main/java/wjdwo1104/hello/boot/spring5boot/controller/GalleryController.java
@@ -2,22 +2,36 @@ package wjdwo1104.hello.boot.spring5boot.controller;
 
 
 
+import lombok.RequiredArgsConstructor;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import wjdwo1104.hello.boot.spring5boot.service.GalleryService;
 
 
 @Controller
 @RequestMapping("/gallery")
+@RequiredArgsConstructor
 public class GalleryController {
-
+    final GalleryService gsrv;
     Logger logger = LogManager.getLogger(GalleryController.class);
 
-    @GetMapping("/list")
-    public String index(){
-        logger.info("gallery 호출");
+    @GetMapping("/list/{cpg}")
+    public String list(Model m, @PathVariable Integer cpg){
+        logger.info("gallery/list 호출");
+        m.addAttribute("gal",gsrv.readGallery(cpg));
+        m.addAttribute("cpg",cpg);//현재페이지
+        m.addAttribute("cntpg",gsrv.countGallery());//총페이지
+        m.addAttribute("stpg",((cpg - 1) / 10)*10+1 );//총페이지
+
+        if(cpg>(int)m.getAttribute("cntpg")){
+            return "redirect:/gallery/list/1";
+
+        }
         return "gallery/list";
     }
 
