7e29c73c4204adebd5f48fd24be9a81960724899
gain00 <wjdwo1104@gmail.com>
2023-07-20 11:17:44 +0900

    갤러리 : Gallery/ GalAttach 클래스 추가

---
+++ src/main/java/wjdwo1104/hello/boot/spring5boot/model/GalAttach.java
@@ -0,0 +1,22 @@
+package wjdwo1104.hello.boot.spring5boot.model;
+
+import lombok.*;
+
+@Setter
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+public class GalAttach {
+
+        private String gano;
+        private String gno;
+        private String fname;
+        private String ftype;
+        private String fsize;
+        private String fdown;
+
+
+
+
+}
---
+++ src/main/java/wjdwo1104/hello/boot/spring5boot/model/Gallery.java
@@ -0,0 +1,23 @@
+package wjdwo1104.hello.boot.spring5boot.model;
+
+import lombok.*;
+
+
+@Setter
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+public class Gallery {
+    private String gno;
+    private String title;
+    private String userid;
+    private String regdate;
+    private String thumbs;
+    private String views;
+    private String contents;
+    private String ipaddr;
+
+    private GalAttach ga;
+
+}
